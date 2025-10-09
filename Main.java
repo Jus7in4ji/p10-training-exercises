a911a7e13ef69c55fb106416f1416456191f9d6a
Justin K A <147239305+Jus7in4ji@users.noreply.github.com>
2025-10-09 12:02:40 +0530

    first commit
    passing data to and from classes , class methods

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.err.println("hello world ");
+    }
+    
+        public int returnlength( String x){
+            return x.length();
+        }
+
+}
---
+++ Second.java
@@ -0,0 +1,11 @@
+
+public class Second{
+    public static void main (String args[]){
+        Main one = new Main();
+        String name = "Justin K A";
+        int l = one.returnlength(name);
+        System.out.println("length = "+l );
+    }
+
+
+}
