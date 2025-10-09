40ec8ba22d98ea63387b5897b14e2c8dc97c80e0
Justin K A <justinajikoikamolath@gmail.com>
2025-10-09 22:05:51 +0530

    Constructors

---
+++ Construct.java
@@ -0,0 +1,17 @@
+public class Construct {
+    static int count = 0;
+
+    Construct(int x){
+        count+=x;
+    }
+    public static void main(String[] args) {
+        Construct c1 = new Construct(3);
+        System.out.println("count + 3 = "+ c1.count);
+
+        Construct c2 = new Construct(4);
+        System.out.println("count + 4 = "+ c2.count);
+
+        Construct c3 = new Construct(5);
+        System.out.println("count + 5 = "+ c3.count);
+    }
+}
---
+++ ConstructorOverLoading.java
@@ -0,0 +1,23 @@
+public class ConstructorOverLoading{
+
+    String name = "Justin";
+
+    ConstructorOverLoading(){
+        System.out.println(this.name);
+    }
+    ConstructorOverLoading(String newname){
+        this.name = newname;
+
+    }
+    ConstructorOverLoading(int x ){
+        System.out.println(x);
+    }
+    public static void main(String args[]){
+        ConstructorOverLoading cl1 = new ConstructorOverLoading(); // prints name 
+        ConstructorOverLoading cl2 = new ConstructorOverLoading(45);// prints number entered
+        ConstructorOverLoading cl3 = new ConstructorOverLoading("Justin K A"); // replaces name 
+        System.out.println("new name : "+cl3.name);
+
+        
+    }
+}
