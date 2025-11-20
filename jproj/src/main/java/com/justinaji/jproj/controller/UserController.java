8a65ee4fbb3425a70e9e4fd7447e69b19867b3a9
Justin K A <justinajikoikamolath@gmail.com>
2025-11-20 09:53:25 +0530

    stores username from jwt in js

--- jproj/src/main/java/com/justinaji/jproj/controller/UserController.java
+++ jproj/src/main/java/com/justinaji/jproj/controller/UserController.java
@@ -1,17 +1,30 @@
 package com.justinaji.jproj.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.justinaji.jproj.dto.Loginform;
 import com.justinaji.jproj.model.users;
+import com.justinaji.jproj.service.JWTService;
 import com.justinaji.jproj.service.user_servicesimpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import io.swagger.v3.oas.annotations.Hidden;
+
 
 
 @RestController
 public class UserController {
     
+    @Autowired
+    private JWTService jwtService; 
+
     private final user_servicesimpl userservice;
     public UserController(user_servicesimpl userservice) {// Constructor injection
         this.userservice = userservice;
@@ -27,6 +40,18 @@ public class UserController {
         
         return userservice.login(user.getUsername(),user.getPassword());
     }
+
+    @Hidden
+    @GetMapping("/getusername")
+    public Map<String, String> getUsername(@RequestParam String token) {
+        try {
+            String username = jwtService.extractUser(token);
+            return Map.of("username", username);
+        } catch (Exception e) {
+            return Map.of("username", "");
+        }
+    }
+
     
 }
 
